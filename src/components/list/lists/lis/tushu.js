5925e3962042af8c4d2560e9743a2968c84494e8
jiahua123 <MrJiaHua@163.com>
2019-02-07 18:06:58 +0800

    组件完成，有待完善

---
+++ src/components/list/lists/lis/tushu.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import './sass/com.scss';
+export default class TuShu extends React.Component{
+	constructor(props){
+		super(props)
+		this.state={
+			arr:[]
+		}
+	}
+	
+	componentWillMount(){	
+		this.loadData();
+	}
+		
+		loadData=()=>{
+			const arr=require('../lisdata/tushu.json');
+//			console.log(arr.data)
+				 this.setState({
+       			arr:arr.data
+     		 })
+		}
+	
+	render(){
+		return <div className="box">
+					<div className="one">
+						<img src={this.state.arr.ad[0].image}/>			
+					</div>
+					<div className="two">
+						{
+						this.state.arr.subCategoryList.map(item=>{
+							return <div key={item.id}>
+								<h5>{item.title}</h5>
+								<ul>						
+										{
+										item.nodeList.map(it=>{										
+										return <li key={it.id}>
+											<img src={it.imageUrl}/>
+											<p>
+											{it.name}
+											</p>
+										</li>
+										})
+										}
+										
+										
+								</ul>
+							</div>
+							
+							
+						})
+						}
+						
+					</div>
+				
+				</div>
+	}
+}
