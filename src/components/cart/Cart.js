82ea378b97160778b9e7a75810900bca730a8772
jiahua123 <MrJiaHua@163.com>
2019-02-08 09:42:43 +0800

    Revert "Revert "完成""
    This reverts commit 008d1a0bdcf945c768da88162290a891802d590e.

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+export default class Cart extends React.Component{
+	constructor(props){
+		super(props)
+		this.state={}
+	}
+	
+	render(){
+		return <div>
+		<h1>CART</h1>
+		</div>
+	}
+}
